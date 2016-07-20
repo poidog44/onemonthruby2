f3b7c73bd2707a3a5b086655dfd6a0b5ae833f66
Stuart Kam <stuart.kam17@gmail.com>
2016-07-19 21:44:27 -1000

    add pins/user association

---
+++ db/migrate/20160720004507_add_column_to_pins.rb
@@ -0,0 +1,5 @@
+class AddColumnToPins < ActiveRecord::Migration[5.0]
+  def change
+    add_column :pins, :user_id, :integer
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160719230724) do
+ActiveRecord::Schema.define(version: 20160720004507) do
 
   create_table "pins", force: :cascade do |t|
     t.string   "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+    t.integer  "user_id"
   end
 
   create_table "posts", force: :cascade do |t|
